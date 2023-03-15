8c27b3b0422bc95eddb7372e6261bcba8dfccb0f
legandcorepower <fgh50608@gmail.com>
2023-03-15 21:07:46 +0800

    add cors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const cors = require('cors');
+
 const db = require('./utils/database');
 
 //const testDB = require('./utils/test-db');
@@ -29,6 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
